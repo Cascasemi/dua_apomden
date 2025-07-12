fd3bfd6ea8b8e15828a716ab7ce03c464549d05c
unknown <kwameroland5@gmail.com>
2025-07-12 23:42:52 +0000

    my first push

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.flutter_dash,
+              size: 100,
+              color: Colors.white, 
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Dua Apomden',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
